acd7fa7619512ba704ed0a6966744f1ebee033b7
Jihyun <bjih1999@naver.com>
2021-07-12 15:33:18 +0900

    feat: 컨트롤러 설정 및 테스트 실습

---
+++ src/main/java/me/jihyun/springMVC/config/WebConfig.java
@@ -0,0 +1,2 @@
+package me.jihyun.springMVC.config;public class WebConfig {
+}
--- src/test/java/me/jihyun/springMVC/Controllers/UserControllerTest.java
+++ src/test/java/me/jihyun/springMVC/Controllers/UserControllerTest.java
@@ -2,8 +2,11 @@ package me.jihyun.springMVC.Controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,15 +14,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(UserController.class )
 public class UserControllerTest {
- 
+
     @Autowired
     MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception{
-        mockMvc.perform(get("hello"))
+        mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
     }
